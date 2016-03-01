b3dd254c1dee3e514fedf3d3b2a432c39b30076d
mylady <closezling@outlook.com>
2016-03-01 21:51:31 +0800

    add product info

---
+++ product_info.go
@@ -0,0 +1,12 @@
+package saints_go_specification
+
+type ProductInfo struct {
+	ProductName   string
+	ProductId     string
+	DisplayName   string
+	ProductType   int
+	VersionNumber string
+	VersionCode   int
+	InstallPath   string
+	OS            int
+}
