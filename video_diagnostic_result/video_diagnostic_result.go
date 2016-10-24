1a5d8e6779c19a771141bfe4c45c44b9916b9dc5
mylady <closezling@outlook.com>
2016-10-24 14:48:35 +0800

    add new enum value

---
+++ video_diagnostic_result/video_diagnostic_result.go
@@ -0,0 +1,16 @@
+package video_diagnostic_result
+
+const (
+	Unknown   = -1
+	Normal    = 0
+	Offline   = 1
+	VideoLost = 2
+	Bright    = 3
+	Color     = 4
+	Contract  = 5
+	Frozen    = 6
+	Blue      = 7
+	Noise     = 8
+	Wave      = 9
+	Roll      = 10
+)
