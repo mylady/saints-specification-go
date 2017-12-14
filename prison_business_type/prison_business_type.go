a8e7ce3a25fc79b58375c9963a62b5b02481a6bd
mylady <closezling@outlook.com>
2017-12-14 15:34:55 +0800

    add new definition

--- prison_business_type/prison_business_type.go
+++ prison_business_type/prison_business_type.go
@@ -4,4 +4,8 @@ const (
 	MeetingArraignRecord   = 0
 	CellTerminalNotify     = 1
 	MeetingArraignFeedback = 2
+	StartFaceRollCall      = 3
+	FaceRollCallFeedback   = 4
+	FaceRollCallCellEnd    = 5
+	StopFaceRollCall       = 6
 )
---
+++ prison_terminal_type/prison_terminal_type.go
@@ -0,0 +1,6 @@
+package prison_terminal_type
+
+const (
+	OutsideTerminal = 0
+	InsideTerminal  = 1
+)
