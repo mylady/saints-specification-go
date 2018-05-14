c1fbc42ea8216cd03433fa2927452b60f7b7caa1
mylady <closezling@outlook.com>
2018-05-14 15:17:15 +0800

    add new field

--- school_check_plan_status/school_check_plan_status.go
+++ school_check_plan_status/school_check_plan_status.go
@@ -3,4 +3,5 @@ package school_check_plan_status
 const (
 	Planning  = 0
 	Published = 1
+	Obsolete  = 2
 )
---
+++ school_check_question_status/school_check_question_status.go
@@ -0,0 +1,7 @@
+package school_check_question_status
+
+const (
+	Editable  = 0
+	Published = 1
+	Obsolete  = 2
+)
