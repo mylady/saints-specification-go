d458a0d7d1ac00f76996d52073d4c5e97b327ffe
mylady <closezling@outlook.com>
2018-05-18 19:11:12 +0800

    add new enum

---
+++ school_map_type/school_map_type.go
@@ -0,0 +1,7 @@
+package school_map_type
+
+const (
+	Image   = 0
+	Gis     = 1
+	GisData = 2
+)
---
+++ school_notice_type/school_notice_type.go
@@ -0,0 +1,7 @@
+package school_notice_type
+
+const (
+	NoticeAll    = 0
+	NoticeSchool = 1
+	NoticeCenter = 2
+)
