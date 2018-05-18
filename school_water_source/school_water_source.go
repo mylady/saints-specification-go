3667b25b44aaa28f395e9d3d44ab7a4707f6d62f
mylady <closezling@outlook.com>
2018-05-18 22:44:48 +0800

    add enum

--- school_water_source/school_water_source.go
+++ school_water_source/school_water_source.go
@@ -4,6 +4,7 @@ const (
 	DirectWater        = 0
 	DirectWaterMachine = 1
 	Cistern            = 2
-	HeatPreserved      = 3
-	Barrel             = 4
+	WaterBox           = 3
+	HeatPreserved      = 4
+	Barrel             = 5
 )
