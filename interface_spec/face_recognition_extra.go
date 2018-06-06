5ceb4a1f0f500191c46e84bd537de3c61cd951ee
mylady <closezling@outlook.com>
2018-06-06 20:37:11 +0800

    add face extra

---
+++ interface_spec/face_recognition_extra.go
@@ -0,0 +1,11 @@
+package interface_spec
+
+type FaceRecognitionExtra struct {
+	SceneImagePath string
+	SceneImage     string
+	FaceImagePath  string
+	FaceImage      string
+	PersonId       string
+	PersonName     string
+	Similarity     float32
+}
