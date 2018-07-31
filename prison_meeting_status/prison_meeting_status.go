b614701054dfc0eaa0b4c263e7a5ac9869071f39
mylady <closezling@outlook.com>
2018-07-31 15:11:06 +0800

    add new enum

--- prison_meeting_status/prison_meeting_status.go
+++ prison_meeting_status/prison_meeting_status.go
@@ -1,10 +1,12 @@
 package prison_meeting_status
 
 const (
-	Waiting  = 0
-	OnGoing  = 1
-	Finished = 2
-	Canceled = 3
-	BringOut = 4
-	BringIn  = 5
+	Waiting     = 0
+	OnGoing     = 1
+	Finished    = 2
+	Canceled    = 3
+	BringOut    = 4
+	BringIn     = 5
+	FingerStart = 6
+	FingerEnd   = 7
 )
