f161caf62c1c1ef1d699c7f18f106d9b3a032efa
mylady <closezling@outlook.com>
2018-11-16 16:24:08 +0800

    add service type dict

--- pkg/web/service.go
+++ pkg/web/service.go
@@ -14,3 +14,11 @@ const (
 	ServiceTypeFileHub
 	ServiceTypeGIS
 )
+
+//ServiceTypeDict :service type name dict
+var ServiceTypeDict = map[int]string{
+	ServiceTypeIdentity: "身份认证服务",
+	ServiceTypeDevice:   "设备信息服务",
+	ServiceTypeFileHub:  "文件管理服务",
+	ServiceTypeGIS:      "地理信息服务",
+}
