482c4c36d3614e352d0b5c2a17193874675301e0
mylady <closezling@outlook.com>
2018-12-04 19:01:56 +0800

    fix code

--- pkg/code/device.go
+++ pkg/code/device.go
@@ -3,32 +3,33 @@ package code
 //ResourceCode
 const (
 	Camera     = 0
-	Door       = 1
-	Alarm      = 2
-	Call       = 3
-	PowerGrid  = 4
-	EnvMonitor = 5
-	RFIDTag    = 6
-	Relay      = 7
-	LED        = 8
+	DoorPoint  = 1
+	Door       = 2
+	Alarm      = 3
+	Call       = 4
+	CallHost   = 5
+	PowerGrid  = 6
+	EnvMonitor = 7
+	RFIDTag    = 8
+	Relay      = 9
+	LED        = 10
 
 	DecoderChannel      = 128
 	MatrixChannel       = 129
 	TVWallChannel       = 130
 	VideoAnalyseChannel = 131
 
-	VM             = 256
-	DM             = 257
-	Matrix         = 258
-	TVWall         = 259
-	AlarmHost      = 260
-	DoorController = 261
-	CallHost       = 262
-	PowerGridHost  = 263
-	EnvMonitorHost = 264
-	UPS            = 265
-	VideoAnalyse   = 266
-	RelayHost      = 267
+	VM        = 256
+	DM        = 257
+	Matrix    = 258
+	TVWall    = 259
+	AlarmHost = 260
+
+	PowerGridHost  = 261
+	EnvMonitorHost = 262
+	UPS            = 263
+	VideoAnalyse   = 264
+	RelayHost      = 265
 )
 
 //AlarmDeviceType
