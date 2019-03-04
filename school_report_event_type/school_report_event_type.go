ba19f2a075e2a0f7cb6cb5fc10abbea4612b188b
mylady <closezling@outlook.com>
2019-03-04 18:36:38 +0800

    add new enum

---
+++ school_report_event_type/school_report_event_type.go
@@ -0,0 +1,13 @@
+package school_report_event_type
+
+const (
+	FireProtection  = 0
+	UnstableFactor  = 1
+	CampusAround    = 2
+	TrafficSafety   = 3
+	FoodSanitation  = 4
+	PublicSentiment = 5
+	Accident        = 6
+	CriminalCase    = 7
+	Others          = 8
+)
