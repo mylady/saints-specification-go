3faaf62bcbaa58532b7def933cefbe51ad27862f
mylady <closezling@outlook.com>
2019-03-20 18:51:06 +0800

    fix meeting status

--- prison_meeting_status/prison_meeting_status.go
+++ prison_meeting_status/prison_meeting_status.go
@@ -1,11 +1,13 @@
 package prison_meeting_status
 
 const (
-	Waiting  = 0
-	OnGoing  = 1
-	Finished = 2
-	Canceled = 3
-	BringOut = 4
-	BringIn  = 5
-	InRoom   = 6
+	Waiting     = 0
+	OnGoing     = 1
+	Finished    = 2
+	Canceled    = 3
+	BringOut    = 4
+	BringIn     = 5
+	FingerBegin = 6
+	FingerEnd   = 7
+	Audit       = 8
 )
