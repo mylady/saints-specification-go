e83ffaf0bca3056970c9ba716448777c7bdea05a
mylady <closezling@outlook.com>
2019-04-30 17:25:19 +0800

    change const name

--- pkg/code/device.go
+++ pkg/code/device.go
@@ -2,47 +2,61 @@ package code
 
 //ResourceCode
 const (
-	Camera     = 0
-	DoorPoint  = 1
-	Door       = 2
-	Alarm      = 3
-	Call       = 4
-	CallHost   = 5
-	PowerGrid  = 6
-	EnvMonitor = 7
-	RFIDTag    = 8
-	Relay      = 9
-	LED        = 10
+	RCCamera     = 0
+	RCDoorPoint  = 1
+	RCDoor       = 2
+	RCAlarm      = 3
+	RCCall       = 4
+	RCCallHost   = 5
+	RCPowerGrid  = 6
+	RCEnvMonitor = 7
+	RCRFIDTag    = 8
+	RCRelay      = 9
+	RCLED        = 10
 
-	DecoderChannel      = 128
-	MatrixChannel       = 129
-	TVWallChannel       = 130
-	VideoAnalyseChannel = 131
+	RCDecoderChannel      = 128
+	RCMatrixChannel       = 129
+	RCTVWallChannel       = 130
+	RCVideoAnalyseChannel = 131
 
-	VM        = 256
-	DM        = 257
-	Matrix    = 258
-	TVWall    = 259
-	AlarmHost = 260
+	RCVM        = 256
+	RCDM        = 257
+	RCMatrix    = 258
+	RCTVWall    = 259
+	RCAlarmHost = 260
 
-	PowerGridHost  = 261
-	EnvMonitorHost = 262
-	UPS            = 263
-	VideoAnalyse   = 264
-	RelayHost      = 265
+	RCPowerGridHost  = 261
+	RCEnvMonitorHost = 262
+	RCUPS            = 263
+	RCVideoAnalyse   = 264
+	RCRelayHost      = 265
 )
 
 //AlarmDeviceType
 const (
-	ManuAlarm     = 0
-	IrAlarm       = 1
-	ElecPerimeter = 2
-	SoundAlarm    = 3
-	SmokeAlarm    = 4
+	AlarmTypeManuAlarm     = 0
+	AlarmTypeIrAlarm       = 1
+	AlarmTypeElecPerimeter = 2
+	AlarmTypeSoundAlarm    = 3
+	AlarmTypeSmokeAlarm    = 4
 )
 
 //CameraDeviceType
 const (
-	Ball = 0
-	Gun  = 1
+	CameraTypeBall = 0
+	CameraTypeGun  = 1
+)
+
+//DeviceStatus
+const (
+	StatusNormal     = 0
+	StatusArm        = 1
+	StatusDisarm     = 2
+	StatusOpen       = 3
+	StatusClose      = 4
+	StatusDisconnect = 5
+	StatusConnect    = 6
+	StatusAlarm      = 7
+	StatusFault      = 8
+	StatusUnknown    = 9
 )
