6b0d77126c77b1661185c715f0a4e9d079cba145
mylady <closezling@outlook.com>
2024-05-31 13:23:43 +0800

    fix bug

--- spec/device_code.go
+++ spec/device_code.go
@@ -15,6 +15,7 @@ const (
 	ResourceCodeRelayModule    = 9
 	ResourceCodeLED            = 10
 	ResourceCodeRFIDLocator    = 11
+	ResourceCodeAc             = 12
 
 	ResourceCodeDecoderChannel      = 128
 	ResourceCodeMatrixChannel       = 129
@@ -33,6 +34,7 @@ const (
 	ResourceCodeVideoAnalyseHost = 265
 	ResourceCodeRelayHost        = 266
 	ResourceCodeVideoGBServer    = 267
+	ResourceCodeAcController     = 268
 )
 
 // DeviceEventType
